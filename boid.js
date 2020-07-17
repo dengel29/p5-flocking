48344b209deb6f3545b7980e943e3146bb658cfd
Dan Engel <dengel29@gmail.com>
2020-07-17 17:01:33 +0800

    cohesion, alignment and separation all implemented, sliders added to control

--- boid.js
+++ boid.js
@@ -4,11 +4,33 @@ class Boid {
     this.velocity = p5.Vector.random2D();
     this.velocity.setMag(random(2, 4));
     this.acceleration = createVector();
+    this.maxForce = 0.2;
+    this.maxSpeed = 4;
   }
 
+  edges() {
+    if (this.position.x > width) {
+      this.position.x = 0;
+    } else if (this.position.x < 0) {
+      this.position.x = width
+    }
+    if (this.position.y > height) {
+      this.position.y = 0;
+    } else if (this.position.y < 0) {
+      this.position.y = height
+    }
+  }
   flock(boids) {
+    let cohesion = this.cohesion(boids)
     let alignment = this.align(boids);
-    this.acceleration = alignment;
+    let separation = this.separation(boids);
+
+    separation.mult(separationSlider.value())
+    cohesion.mult(cohesionSlider.value())
+    alignment.mult(alignmentSlider.value())
+    this.acceleration.add(separation)
+    this.acceleration.add(cohesion);
+    this.acceleration.add(alignment)
   }
 
   align(boids) {
@@ -29,13 +51,71 @@ class Boid {
     }
     if (total > 0) {
       steeringForce.div(total)
+      steeringForce.setMag(this.maxSpeed)
       steeringForce.sub(this.velocity)
+      steeringForce.limit(this.maxForce)
     }
     return steeringForce;
   }
+
+  cohesion(boids) {
+    let perceptionRadius = 50
+    let steeringForce = createVector();
+    let total = 0;
+    for (let other of boids) {
+      let d = dist(
+        this.position.x,
+        this.position.y,
+        other.position.x,
+        other.position.y
+      );
+      if (d < perceptionRadius && other != this) {
+        steeringForce.add(other.position)
+        total++
+      }
+    }
+    if (total > 0) {
+      steeringForce.div(total)
+      steeringForce.sub(this.position)
+      // steeringForce.setMag(this.maxSpeed)
+      steeringForce.sub(this.velocity)
+      steeringForce.limit(this.maxForce)
+    }
+    return steeringForce;
+  }
+
+  separation(boids) {
+    let perceptionRadius = 50
+    let steeringForce = createVector();
+    let total = 0;
+    for (let other of boids) {
+      let d = dist(
+        this.position.x,
+        this.position.y,
+        other.position.x,
+        other.position.y
+      );
+      if (d < perceptionRadius && other != this) {
+        let diff = p5.Vector.sub(this.position, other.position)
+        diff.div(d)
+        steeringForce.add(diff)
+        total++
+      }
+    }
+    if (total > 0) {
+      steeringForce.div(total)
+      steeringForce.setMag(this.maxSpeed)
+      steeringForce.sub(this.velocity)
+      steeringForce.limit(this.maxForce)
+    }
+    return steeringForce;
+  }
+
   update() {
     this.position.add(this.velocity)
     this.velocity.add(this.acceleration)
+    this.velocity.limit(this.maxSpeed)
+    this.acceleration.set(0, 0)
   }
   show() {
     strokeWeight(8);
--- logic.js
+++ logic.js
@@ -1,7 +1,12 @@
 let flock = [];
 
+let cohesionSlider, alignmentSlider, separationSlider;
+
 function setup() {
-  createCanvas(640, 360)
+  createCanvas(640, 360);
+  alignmentSlider = createSlider(0, 5, 1, 0.1);
+  cohesionSlider = createSlider(0, 5, 1, 0.1);
+  separationSlider = createSlider(0, 5, 1, 0.1);
   for (i = 0; i < 100; i++) {
     flock.push(new Boid())
   }
@@ -11,6 +16,7 @@ function setup() {
 function draw() {
   background(41)
   for (let boid of flock) {
+    boid.edges()
     boid.flock(flock)
     boid.update()
     boid.show()
